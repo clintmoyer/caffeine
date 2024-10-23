a88054f3512829d1c66dad8f843ec34fdc489d78
Clint Moyer <contact@clintmoyer.com>
2024-10-22 21:39:09 -0700

    Add macOS menu bar app logic and custom status button
    Implement the logic to add the app icon in the macOS menu bar. Introduce
    a custom status button for user interaction and toggle functionality for
    Caffeine activation.

--- caffeine.swift
+++ caffeine.swift
@@ -16,6 +16,7 @@ import Cocoa
 
 class CaffeineApp: NSObject, NSApplicationDelegate {
     var statusItem: NSStatusItem!
+    var isActive = false  // New property to track activation status
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         setupMenuBarIcon()
@@ -23,8 +24,14 @@ class CaffeineApp: NSObject, NSApplicationDelegate {
 
     func setupMenuBarIcon() {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
         if let button = statusItem.button {
-            button.image = NSImage(named: "inactive")
+            button.image = NSImage(named: "inactive")  // Default icon
+
+            // Create custom button and assign action for toggling Caffeine
+            let customView = CustomStatusButton(frame: button.bounds)
+            customView.target = self
+            button.addSubview(customView)
         }
 
         let menu = NSMenu()
@@ -32,8 +39,39 @@ class CaffeineApp: NSObject, NSApplicationDelegate {
         statusItem.menu = menu
     }
 
+    @objc func toggleCaffeine() {
+        isActive.toggle()  // Toggle the active state
+        updateIcon()       // Update icon based on the current state
+    }
+
+    func updateIcon() {
+        if let button = statusItem.button {
+            if isActive {
+                button.image = NSImage(named: "active")   // Active icon
+            } else {
+                button.image = NSImage(named: "inactive") // Inactive icon
+            }
+        }
+    }
+
     @objc func quitApp() {
         NSApplication.shared.terminate(self)
+    }
+}
+
+class CustomStatusButton: NSView {
+    var target: CaffeineApp?
+
+    override func mouseDown(with event: NSEvent) {
+        if event.type == .leftMouseDown {
+            target?.toggleCaffeine()  // Toggle state on left-click
+        }
+    }
+
+    override func rightMouseDown(with event: NSEvent) {
+        if let statusItem = target?.statusItem {
+            statusItem.menu?.popUp(positioning: nil, at: NSEvent.mouseLocation, in: nil)  // Show menu on right-click
+        }
     }
 }
 
