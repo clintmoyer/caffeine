01f2419e827a96f2cfa5e2e326a25933215d1fc4
Clint Moyer <contact@clintmoyer.com>
2025-12-07 23:17:55 -0800

    Project restructure

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "Caffeine",
+    platforms: [.macOS(.v11)],
+    products: [
+        .executable(name: "Caffeine", targets: ["caffeine"]),
+    ],
+    targets: [
+        .executableTarget(
+            name: "caffeine",
+            dependencies: [],
+            path: "Sources/caffeine"
+        ),
+        .testTarget(
+            name: "caffeineTests",
+            dependencies: ["caffeine"],
+            path: "Tests/caffeineTests"
+        ),
+    ]
+)
